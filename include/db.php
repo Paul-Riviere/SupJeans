04137f3a9924875a63c64039633faf458ddfbfb8
Paul-Riviere <38280859+Paul-Riviere@users.noreply.github.com>
2021-12-23 14:25:45 +0100

    Update db.php

--- include/db.php
+++ include/db.php
@@ -4,7 +4,7 @@ include('conf.php');
 // Connexion DB
 
 try {
-    $pdo = new PDO('mysql:host=' . HOST . 'dbname=' . NOM_DB, UTILISATEUR_DB, MDP_DB);
+    $pdo = new PDO("mysql:host=$HOST_DB;dbname=$NOM_DB", $UTILISATEUR_DB, $MDP_DB);
 } catch (PDOException $e) {
     die("Error ! : " . $e->getMessage());
 }
